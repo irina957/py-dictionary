9f2b64d41f011bb95215910fb3c74cf4200d6c2d
Iryna <irakirvas78@gmail.com>
2025-03-27 18:39:38 +0300

    Implement py-dictionary

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ class Dictionary:
         for k, v in bucket:
             if k == key:
                 return v
-        raise KeyError(f"Key {key} not found")
+        raise KeyError("Key not found")
 
     def __len__(self) -> int:
         return self.size
